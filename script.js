45c63c80a9db09a2ebfe4368f30829061af174d5
dorjeezzz <dj.namjyal@gmail.com>
2024-06-18 13:30:14 -0700

    Made intial 16x16 grid, need to update the custom grid and implement delete function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const container = document.querySelector("#container");
+let container = document.querySelector("#container");
+const cool = "black";
 
-for (let i = 0; i < 256; i++){
-    let square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+for (let i = 0; i < 16; i++){
+    for (let j = 0; j < 16; j++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.addEventListener("mouseover", (e) => {
+            square.style.backgroundColor = cool;
+        })
+        container.appendChild(square);
+    }
 }
-
