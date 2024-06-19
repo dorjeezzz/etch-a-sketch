69ea1ee4ae973347b2f024fb20e5162beecf487d
dorjeezzz <dj.namjyal@gmail.com>
2024-06-19 11:51:37 -0700

    made the grid input manual, need to make deleteGrid function then should be done

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let container = document.querySelector("#container");
 const cool = "black";
 
-for (let i = 0; i < 16; i++){
-    for (let j = 0; j < 16; j++){
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseover", (e) => {
-            square.style.backgroundColor = cool;
-        })
-        container.appendChild(square);
+function create(n){
+    for (let i = 0; i < n; i++){
+        for (let j = 0; j < n; j++){
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", (e) => {
+                square.style.backgroundColor = cool;
+            })
+            container.appendChild(square);
+        }
     }
+}
+
+let dimensions;
+let btn = document.querySelector("#change_d");
+btn.addEventListener("click", (e)=>{
+    dimensions = prompt("Input Squares per Side");
+})
+
+function deleteGrid(){
+    if (dimensions){
+        //delete the grid
+    }
+    else return;
 }
